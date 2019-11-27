f34c8a9b50e02638f94f8787594d87fd3beac049
lm <2679609156@qq.com>
2019-11-27 14:21:33 +0800

    增加部门类

---
+++ src/main/java/com/csdj/crmproject/crmproject/entity/Dept.java
@@ -0,0 +1,16 @@
+/*
+ * 文 件 名：
+ * 版权(c) 2018-xxx公司 xxx项目组：
+ * 版 本 号：
+ */
+package com.csdj.crmproject.crmproject.entity;
+
+/**
+ * @author 凌敏
+ * @version 1.0
+ * @date 2019/11/27 14:18
+ * @description: 部门
+ */
+public class Dept {
+    private String name;
+}
