2cf24c114f4507ad2a55127239f496b734be408d
ahaoi123 <2991744603@qq. com>
2020-02-06 19:58:27 +0800

    111

---
+++ src/main/java/com/csdj/crmproject/crmproject/entity/CustomerManage/ContactManage/test.java
@@ -0,0 +1,11 @@
+package com.csdj.crmproject.crmproject.entity.CustomerManage.ContactManage;
+
+/**
+ * @BelongsProject: crmproject
+ * @BelongsPackage: com.csdj.crmproject.crmproject.entity.customermanagement
+ * @Author: admin
+ * @CreateTime: 2020-02-06 15:02
+ * @Description: 占位置用的
+ */
+public class test {
+}
