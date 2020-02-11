268ca1eeb993d580f0bb7f8922630b40c52e67cb
user.name <2863622669@qq.com>
2020-02-11 20:04:07 +0800

    销售图表

--- src/main/java/com/csdj/crmproject/crmproject/controller/Instrument/SectionInstrument/SectionInstrumentController.java
+++ src/main/java/com/csdj/crmproject/crmproject/controller/Instrument/SectionInstrument/SectionInstrumentController.java
@@ -1,4 +1,25 @@
 package com.csdj.crmproject.crmproject.controller.Instrument.SectionInstrument;
 
+import com.csdj.crmproject.crmproject.service.Instrument.SectionInstrument.SectionInstrumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.sound.midi.Instrument;
+
+/**
+ * @ClassName SectionInstrumentController
+ * @Description: SectionInstrumentController
+ * @Author 刘时钟
+ * @Date 2020/2/10
+ * @Version V1.0
+ **/
+@Controller
 public class SectionInstrumentController {
+    @Autowired
+    private SectionInstrumentService service;
+    @RequestMapping("/showSectionInstrument")
+    public String showSectionInstrument() {
+        return "/Instrument/SectionInstrument/SectionInstrument";
+    }
 }
