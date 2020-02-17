46cc1701c67ea0782aae2108c9f6cf56ed8ecd78
zuinian <1823579928@qq.com>
2020-02-17 17:45:56 +0800

    订单管理

--- src/main/java/com/csdj/crmproject/crmproject/controller/Instrument/MySalesInstrument/MySalesInstrumentController.java
+++ src/main/java/com/csdj/crmproject/crmproject/controller/Instrument/MySalesInstrument/MySalesInstrumentController.java
@@ -48,10 +48,24 @@ public class MySalesInstrumentController {
         CusSalesTarget cusSalesTarget = mySalesInstrumentService.getOrder(userId, year);
         Map<String, Object> map = new HashMap<String, Object>();
         //本月销售业绩
+        double dianasOrder = 0;
+        Object order = null;
         Map<Object, String> map1 = mySalesInstrumentService.dianasOrder(userId);
-        Object order = map1.get("order_Amount_remitted");
+        if (map1 == null) {
+            dianasOrder = 0;
+        } else {
+            order = map1.get("order_Amount_remitted");
+        }
+
+        Object order11 = null;
+        double dianasContract = 0;
         Map<Object, String> map11 = mySalesInstrumentService.dianasContract(userId);
-        Object order11 = map11.get("contract_saleroom");
+        if (map11 == null) {
+            dianasContract = 0;
+        } else {
+            order11 = map11.get("contract_saleroom");
+        }
+
 
         double predict = 0;
         switch (month) {
@@ -92,15 +106,27 @@ public class MySalesInstrumentController {
                 predict = cusSalesTarget.getCusSalesTarDecember();
                 break;
         }
-        double i = Double.parseDouble(order.toString()) / predict * 100;//订单
-        double i11 = Double.parseDouble(order11.toString()) / predict * 100;//合同
+        dianasOrder = Double.parseDouble(String.valueOf(order == null ? 0 : Double.parseDouble(order.toString()))) / predict * 100;//订单
+        dianasContract = Double.parseDouble(String.valueOf(order11 == null ? 0 : Double.parseDouble(order11.toString()))) / predict * 100;//合同
         //本季销售业绩
         int month1 = date.get(Calendar.MONTH) + 1;
+        double seasonOrder = 0;
+        Object order1 = null;
         Map<Object, String> map2 = mySalesInstrumentService.seasonOrder(userId);
-        Object order1 = map2.get("order_Amount_remitted");
+        if (map2 == null) {
+            seasonOrder = 0;
+        } else {
+            order1 = map2.get("order_Amount_remitted");
+        }
 
+        double seasonContract = 0;
+        Object order22 = null;
         Map<Object, String> map22 = mySalesInstrumentService.seasonContract(userId);
-        Object order22 = map22.get("contract_saleroom");
+        if (map22 == null) {
+            seasonContract = 0;
+        } else {
+            order22 = map22.get("contract_saleroom");
+        }
 
         double predict1 = 0;
         if (month1 <= 3) {
@@ -115,30 +141,44 @@ public class MySalesInstrumentController {
         if (month1 >= 10 && month1 <= 12) {
             predict1 = cusSalesTarget.getCusSalesTarOctober() + cusSalesTarget.getCusSalesTarNovember() + cusSalesTarget.getCusSalesTarDecember();
         }
-        double i1 = Double.parseDouble(order1.toString()) / predict1 * 100;
-        double i22 = Double.parseDouble(order22.toString()) / predict1 * 100;
+        seasonOrder = Double.parseDouble(String.valueOf(order1 == null ? 0 : Double.parseDouble(order1.toString()))) / predict1 * 100;
+        seasonContract = Double.parseDouble(String.valueOf(order22 == null ? 0 : Double.parseDouble(order22.toString()))) / predict1 * 100;
 
 
         //本年销售业绩
+        double yearOrder = 0;
+        Object order2 = null;
         Map<Object, String> map3 = mySalesInstrumentService.yearOrder(userId);
-        Object order2 = map3.get("order_Amount_remitted");
+        if (map3 == null) {
+            yearOrder = 0;
+        } else {
+            order2 = map3.get("order_Amount_remitted");
+        }
+
+        double yearContract = 0;
+        Object order33 = null;
         Map<Object, String> map33 = mySalesInstrumentService.yearContract(userId);
-        Object order33 = map33.get("contract_saleroom");
+        if (map33 == null) {
+            yearContract = 0;
+        } else {
+            order33 = map33.get("contract_saleroom");
+
+        }
+
         double predict2 = 0;
         predict2 = cusSalesTarget.getCusSalesTarJanuary() + cusSalesTarget.getCusSalesTarFebruary() + cusSalesTarget.getCusSalesTarMarch() +
                 cusSalesTarget.getCusSalesTarApril() + cusSalesTarget.getCusSalesTarMay() + cusSalesTarget.getCusSalesTarJun() +
                 cusSalesTarget.getCusSalesTarJuly() + cusSalesTarget.getCusSalesTarAugust() + cusSalesTarget.getCusSalesTarSeptember() +
                 cusSalesTarget.getCusSalesTarOctober() + cusSalesTarget.getCusSalesTarNovember() + cusSalesTarget.getCusSalesTarDecember();
-        double i2 = Double.parseDouble(order2.toString()) / predict2 * 100;
-        double i33 = Double.parseDouble(order33.toString()) / predict2 * 100;
+        yearOrder = Double.parseDouble(String.valueOf(order2 == null ? 0 : Double.parseDouble(order2.toString()))) / predict2 * 100;
+        yearContract = Double.parseDouble(String.valueOf(order33 == null ? 0 : Double.parseDouble(order33.toString()))) / predict2 * 100;
+        map.put("dianasOrder", dianasOrder > 100 ? 100 : fun(dianasOrder));
+        map.put("seasonOrder", seasonOrder > 100 ? 100 : fun(seasonOrder));
+        map.put("yearOrder", yearOrder > 100 ? 100 : fun(yearOrder));
 
-        map.put("dianasOrder", i > 100 ? 100 : fun(i));
-        map.put("seasonOrder", i1 > 100 ? 100 : fun(i1));
-        map.put("yearOrder", i2 > 100 ? 100 : fun(i2));
-
-        map.put("dianasContract",i11 > 100 ? 100 : fun(i11));
-        map.put("seasonContract",i22 > 100 ? 100 : fun(i22));
-        map.put("yearContract",i33 > 100 ? 100 : fun(i33));
+        map.put("dianasContract", dianasContract > 100 ? 100 : fun(dianasContract));
+        map.put("seasonContract", seasonContract > 100 ? 100 : fun(seasonContract));
+        map.put("yearContract", yearContract > 100 ? 100 : fun(yearContract));
         return map;
     }
 
@@ -161,6 +201,52 @@ public class MySalesInstrumentController {
         Map<Object, String> map2 = mySalesInstrumentService.contract(userId);
         list.add(map1);
         list.add(cusSalesTarget);
+        list.add(map2);
+        return list;
+    }
+
+    /**
+     * 查询回款统计图
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping("getReceiPlan")
+    @ResponseBody
+    public Map<String, Double> getReceiPlan(HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        String userId = Long.toString(user.getPkUserId());
+        Map<String, Double> map = new HashMap<String, Double>();
+        Double dianasOrReceiPlan = mySalesInstrumentService.dianasOrReceiPlan(userId) == null ? 0.0 : mySalesInstrumentService.dianasOrReceiPlan(userId);
+        Double dianasReceiPlan = mySalesInstrumentService.dianasRecei_plan(userId) == null ? 0.0 : mySalesInstrumentService.dianasRecei_plan(userId);
+
+        Double seasonOrReceiPlan = mySalesInstrumentService.seasonOrReceiPlan(userId) == null ? 0.0 : mySalesInstrumentService.seasonOrReceiPlan(userId);
+        Double seasonReceiPlan = mySalesInstrumentService.seasonRecei_plan(userId) == null ? 0.0 : mySalesInstrumentService.seasonRecei_plan(userId);
+
+        Double yearOrReceiPlan = mySalesInstrumentService.yearOrReceiPlan(userId) == null ? 0.0 : mySalesInstrumentService.yearOrReceiPlan(userId);
+        Double yearReceiPlan = mySalesInstrumentService.yearRecei_plan(userId) == null ? 0.0 : mySalesInstrumentService.yearRecei_plan(userId);
+
+        map.put("dianasReceiPlan",fun(dianasOrReceiPlan/dianasReceiPlan*100));
+        map.put("seasonReceiPlan",fun(seasonOrReceiPlan/seasonReceiPlan*100));
+        map.put("yearReceiPlan",fun(yearOrReceiPlan/yearReceiPlan*100));
+        return map;
+    }
+
+    /**
+     * 查询统计图月度订单
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping("getYearPercentage")
+    @ResponseBody
+    public List<Object> getYearPercentage(HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        String userId = Long.toString(user.getPkUserId());
+        List<Object> list = new ArrayList<Object>();
+        Map<String, Double> map1 = mySalesInstrumentService.yearAccomplish(userId);
+        Map<String, Double> map2 = mySalesInstrumentService.yearAnswerAccomplish(userId);
+        list.add(map1);
         list.add(map2);
         return list;
     }
